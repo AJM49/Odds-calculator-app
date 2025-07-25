a7e0449e8ac1c43a918b0d2ebb773125ff906728
A Murph <albert.murph@gmail.com>
2025-07-25 17:11:26 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,22 @@
-self.addEventListener('install', event => {
-  console.log('🔧 Service Worker installing...');
-  self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("odds-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json",
+        "./favicon.ico"
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  console.log('🚀 Service Worker ready!');
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
 });
-
-self.addEventListener('fetch', event => {
-  // You can cache resources here later
-});
-
-
