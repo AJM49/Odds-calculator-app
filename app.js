e1a533e12a4394f533c277352c4e4def5f026aa3
A Murph <albert.murph@gmail.com>
2025-08-03 21:49:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Ensure Firebase SDK is imported before this script, e.g.:
-// <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-// <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
-// <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+ <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+ <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+ <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
 
 // Firebase Config
 const firebaseConfig = {
